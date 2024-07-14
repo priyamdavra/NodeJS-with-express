06af8df92cfd65ea93b5768817cf36d12cde67a2
priyamdavra <priyamdavra@gmail.com>
2024-07-14 10:00:26 +0530

    PARAM Middleware used

--- Controllers/moviesController.js
+++ Controllers/moviesController.js
@@ -2,6 +2,21 @@ const fs = require('fs');
 
 let movies = JSON.parse(fs.readFileSync('./data/movies.json'));
 
+
+//PARAM MIDDLEWARE USED
+exports.checkId = (req, res, next, value) => {
+    console.log('Movie id is : '+ value);
+
+    let movieToUpdate = movies.find(el => el.id === value *1 );
+    if(!movieToUpdate){
+        return res.status(404).json({
+             status: "fail",
+             message : "No movie object with Id "+value+" is found"
+         })
+     }
+    next();
+}
+
 //ROUTE HANDELER FUNCTION
 
 exports.getallMovie =  (req,res) =>{
@@ -16,21 +31,13 @@ exports.getallMovie =  (req,res) =>{
 }
 
 exports.getMovie = (req,res) =>{
-    // console.log(req.params);
-
-    //CONVERTING ID TO INTEGER TYPE
+     //CONVERTING ID TO INTEGER TYPE
     const id = req.params.id * 1;
 
     //FIDN MOVIE BASED ON PARAMETER
     let movie = movies.find(el => el.id===id);
 
-    if(!movie){
-       return res.status(404).json({
-            status: "fail",
-            message : "Movie with Id "+id+" is not found"
-        })
-    }
-
+    
     //SEND MOVIE IN THE RESPONCE
     res.status(200).json({
         status: "sucess",
@@ -59,12 +66,7 @@ exports.createMovie = (req,res) =>{
 exports.updateMovie = (req,res) =>{
     let id = req.params.id * 1;
     let movieToUpdate = movies.find(el => el.id===id);
-    if(!movieToUpdate){
-        return res.status(404).json({
-             status: "fail",
-             message : "No movie object with Id "+id+" is found"
-         })
-     }
+  
     let index = movies.indexOf(movieToUpdate);
 
     Object.assign(movieToUpdate, req.body);
@@ -84,12 +86,7 @@ exports.updateMovie = (req,res) =>{
 exports.deleteMovie = (req,res) =>{
     let id = req.params.id * 1;
     let movieToDelete = movies.find(el => el.id===id);
-    if(!movieToDelete){
-        return res.status(404).json({
-             status: "fail",
-             message : "No movie object with Id "+id+" is found for delete."
-         })
-     }
+
     const index = movies.indexOf(movieToDelete);
     
     movies.splice(index,1);
