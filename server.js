d1053431620dd01430f30e9eedd9c113bbeb8497
priyamdavra <priyamdavra@gmail.com>
2024-07-23 22:09:51 +0530

    Database Connection

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
 
 const app = require('./app');
 
 console.log(process.env);
+
+mongoose.connect(process.env.CONN_STR,{
+    useNewUrlParser: true
+}).then((conn) => {
+    //console.log(conn);
+    console.log('DB Connection Sucessful');
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port,() => {
