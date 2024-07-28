54cb60cfb371fa10c714667b68e4ddfcb1b25a8a
priyamdavra <priyamdavra@gmail.com>
2024-07-28 09:50:55 +0530

    Import movies data in database with more fields

--- Models/movieModel.js
+++ Models/movieModel.js
@@ -4,16 +4,54 @@ const movieSchema = new mongoose.Schema({
     name:{
         type: String,
         requried: [true, 'Name is required field!'],
-        unique: true
+        unique: true,
+        trim: true
     },
-    description: String,
+    description: {
+        type: String,
+        requried: [true, 'Description is required field!'],
+        trim: true
+    },
     duration:{
         type: Number,
         required: [true,'Duration is required field!']
     },
     rating: {
+        type: Number
+    },
+    totalRating:{
+        type: Number
+    },
+    releaseYear:{
         type: Number,
-        default: 1.0
+        required: [true,'Release year is required field!']
+    },
+    releaseDate:{
+        type: Date
+    },
+    createdAt:{
+        type:Date,
+        default: Date.now()
+    },
+    genres:{
+        type:[String],
+        required: [true,'Genres is required field!']
+    },
+    director:{
+        type:[String],
+        required: [true,'Director is required field!']
+    },
+    coverImage:{
+        type: String,
+        required: [true,'Cover image is required field!']
+    },
+    actors:{
+        type:[String],
+        required: [true,'Actor is required field!']
+    },
+    price:{
+        type: Number,
+        required: [true,'Price is required field!']
     }
 });
 
